b417cb47e81305cf1eeceaae5da558db2f78e4fa
poly000 <1348292515a@gmail.com>
2024-03-15 13:03:48 +0800

    feat: initial impl

---
+++ src/main.rs
@@ -0,0 +1,113 @@
+mod path;
+use std::{
+    collections::{BTreeMap, HashMap},
+    env,
+    error::Error,
+    fs,
+    path::PathBuf,
+};
+
+use path::BackupFile;
+
+const PREVIOUS_MONTH_KEEP_FIRST: usize = 2;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let target_dir = env::args().nth(1).ok_or_else(|| {
+        let my_path = env::args().nth(0).unwrap();
+        format!("{my_path} <TARGET DIR>")
+    })?;
+    let target_dir = PathBuf::from(target_dir);
+
+    let mut target_backupfiles: HashMap<String, Vec<BackupFile>> = HashMap::new();
+    for backup_file in target_dir
+        .read_dir()?
+        .filter_map(Result::ok)
+        .map(|e| e.path())
+        .filter(|p| p.is_file())
+        .filter_map(BackupFile::new)
+    {
+        target_backupfiles
+            .entry(backup_file.target.clone())
+            .or_default()
+            .push(backup_file);
+    }
+
+    for backup_files in target_backupfiles.into_values() {
+        let delete_list = backups_to_delete(backup_files.into_iter())?;
+        for path in delete_list.map(|bf| bf.path) {
+            if let Err(e) = fs::remove_file(&path) {
+                println!(
+                    "Error occured when deleting {}: {e}",
+                    path.to_string_lossy()
+                )
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn backups_to_delete(
+    backup_files: impl Iterator<Item = BackupFile>,
+) -> Result<impl Iterator<Item = BackupFile>, Box<dyn Error>> {
+    let mut backfile_map: BTreeMap<u32, Vec<BackupFile>> = BTreeMap::new();
+
+    for (year, backup_file) in backup_files.map(|back| (back.year, back)) {
+        backfile_map.entry(year).or_default().push(backup_file);
+    }
+
+    // sort backup files by timestamps in filename
+    backfile_map.iter_mut().for_each(|(_, backup_files)| {
+        backup_files.sort();
+    });
+
+    let current_year = backfile_map
+        .keys()
+        .last()
+        .expect("no backup file was found")
+        .clone();
+
+    let current_month = backfile_map
+        .entry(current_year)
+        .or_default()
+        .last()
+        .unwrap()
+        .month;
+
+    // keep every first backup of a year
+    backfile_map.iter_mut().for_each(|(_, b)| {
+        b[0].keep = true;
+    });
+
+    // keep latest 2 archives of previous months in this year
+    // only keep the first archive of a month
+    backfile_map.entry(current_year).and_modify(|b| {
+        let mut month_seen: BTreeMap<_, &mut BackupFile> = BTreeMap::new();
+        for backup_file in b
+            .iter_mut()
+            .rev()
+            .skip_while(|bf| bf.month == current_month)
+        {
+            month_seen.insert(backup_file.month, backup_file);
+        }
+
+        for (_, backup_file) in month_seen.into_iter().rev().take(PREVIOUS_MONTH_KEEP_FIRST) {
+            backup_file.keep = true;
+        }
+    });
+
+    // keep every archive of this month
+    backfile_map.entry(current_year).and_modify(|b| {
+        b.iter_mut()
+            .filter(|b| b.month == current_month)
+            .for_each(|b| {
+                b.keep = true;
+            });
+    });
+
+    Ok(backfile_map
+        .into_iter()
+        .map(|(_, files)| files.into_iter())
+        .flatten()
+        .filter(|b| b.keep == false))
+}
